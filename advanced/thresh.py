53357a0b80d245dada073415db816563788eb4c5
jvillar <mono40mono@gmail.com>
2023-10-09 12:29:10 -0500

    thresholding

--- advanced/thresh.py
+++ advanced/thresh.py
@@ -13,5 +13,8 @@ cv.imshow('Simple Threshold', thresh)
 threshold, thresh_inv = cv.threshold(gray, 150, 255, cv.THRESH_BINARY_INV)
 cv.imshow('Simple Threshold Inverse', thresh_inv)
 
+#Adaptative threshold
+adaptative_thresh = cv.adaptiveThreshold(gray, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY, 11,3)
+cv.imshow('Adaptative threshold' , adaptative_thresh)
 
 cv. waitKey(0)
