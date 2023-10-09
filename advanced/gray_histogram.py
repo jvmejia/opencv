d2b381914d0e5e6e7a916c6b58c1d02e5d6a2f36
jvillar <mono40mono@gmail.com>
2023-10-09 11:24:48 -0500

    gray and rgb histograms

---
+++ advanced/gray_histogram.py
@@ -0,0 +1,34 @@
+import cv2 as cv 
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+img = cv.imread('Photos/cats 2.jpg')
+cv.imshow("gatos", img)
+
+
+blank =  np.zeros(img.shape[:2], dtype='uint8')
+
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+cv.imshow('Grises', gray)
+
+circle = cv.circle(blank, (img.shape[1]//2, img.shape[0]//2), 100, 255, -1)
+
+
+mask = cv.bitwise_and(gray, gray, mask = circle)
+cv.imshow('Mask', mask)
+#Histograma escala grises
+gray_hist = cv.calcHist([gray], [0], mask, [256], [0,256])
+
+plt.figure()
+plt.title("Histograma escala de grises")
+plt.xlabel('Bins')
+plt.ylabel('# of pixels')
+plt.plot(gray_hist)
+plt.xlim([0,256])
+plt.show()
+
+
+
+
+cv.waitKey(0)
